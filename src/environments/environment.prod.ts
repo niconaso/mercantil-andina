6298e5cf3243a9f436e9b8765493acf51d948bce
Nicolas Naso <nicolasna@arionkoder.com>
2021-11-13 15:25:42 -0300

    feat(environment): add base api endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  mercantilAndina: {
+    api: 'https://servicios.qamercantilandina.com.ar/api/v1',
+    apiMock: 'https://servicios.qamercantilandina.com.ar/api_mock_frontend/v1',
+  },
+  gobAr: {
+    geoRefApi: 'https://apis.datos.gob.ar/georef/api',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mercantilAndina: {
+    api: 'https://servicios.qamercantilandina.com.ar/api/v1',
+    apiMock: 'https://servicios.qamercantilandina.com.ar/api_mock_frontend/v1',
+  },
+  gobAr: {
+    geoRefApi: 'https://apis.datos.gob.ar/georef/api',
+  },
 };
 
 /*
